9d22c1fc4ad257bc96128a182e7aaa2eafedece7
Teja <starteja.007@gmail.com>
2022-03-21 01:29:48 +0530

    Add event listeners

--- index.js
+++ index.js
@@ -10,6 +10,36 @@ while(!validateEmail(email)){
     var email=prompt("Please enter valid Email", "batman@gmail.com");
 }
 
+function appleStore(){
+  window.open("https://www.apple.com/in/app-store/");
+};
 
+function googleStore(){
+  window.open("https://play.google.com/store/apps/details?id=com.google.android.apps.podcasts");
+}
+
+function youTube(){
+  window.open("https://www.youtube.com/channel/UCjzJ8GctyZQJzN-MBncaTYg");
+}
+
+function signUp(){
+  window.open("https://retail.onlinesbi.com/personal/");
+}
+
+function twitter(){
+  window.open("https://twitter.com/i/flow/login?input_flow_data=%7B%22requested_variant%22%3A%22eyJsYW5nIjoiZW4ifQ%3D%3D%22%7D");
+}
+
+function instagram(){
+  window.open("https://www.instagram.com/accounts/login/");
+}
+
+function facebook(){
+  window.open("https://www.facebook.com/");
+}
+
+function mail(){
+  window.open('mailto:starteja.007@gmail.com');
+}
 
 
