9c530657f3e48abbc5137ab4e951f57dba86a9aa
Teja <starteja.007@gmail.com>
2022-03-21 01:49:07 +0530

    Add node file for serving in heroku

---
+++ app.js
@@ -0,0 +1,17 @@
+const { Console } = require("console");
+const express= require("express");
+const app=express();
+const port=process.env.PORT || 3000;
+
+app.use(express.static('.'))
+app.use(express.static("public"));
+
+app.get("/",(req,res)=>{
+
+    res.sendFile('index.html', {root: __dirname })
+
+});
+
+app.listen(port,()=>{
+    console.log("Listening");
+})
