fda1e904df3513afee9cb715528a9a842b8ba5ea
Teja <starteja.007@gmail.com>
2022-03-20 23:16:58 +0530

    Validate email

---
+++ index.js
@@ -0,0 +1,15 @@
+var email=prompt("Please enter your Email", "batman@gmail.com");
+
+const validateEmail = (email) => {
+    return email.match(
+      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+};
+
+while(!validateEmail(email)){
+    var email=prompt("Please enter valid Email", "batman@gmail.com");
+}
+
+
+
+
